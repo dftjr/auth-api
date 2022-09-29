a6e76bdc5ccc836b7c26b9395ee3c0951a303e10
DAVE.TUSIA@GMAIL.COM~ <DAVE.TUSIA@GMAIL.COM>
2022-09-29 14:16:23 -0700

    Initial post. Code completed

---
+++ src/server.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// 3rd Party Resources
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// Esoteric Resources
+const errorHandler = require('./error-handlers/500.js');
+const notFound = require('./error-handlers/404.js');
+const authRoutes = require('./routes/routes.js');
+const logger = require('./middleware/logger.js');
+
+const v1Routes = require('./routes/v1.js');
+const bearer = require('./middleware/bearer.js');
+
+// Prepare the express app
+const app = express();
+
+// App Level MW
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(logger);
+app.use('/api/v1', v1Routes);
+app.use('/api/v2',bearer, v1Routes);
+
+
+// Routes
+app.use(authRoutes);
+
+// Catchalls
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = {
+  server: app,
+  start: port => {
+    if (!port) { throw new Error('Missing Port'); }
+    app.listen(port, () => console.log(`Listening on ${port}`));
+  },
+};
